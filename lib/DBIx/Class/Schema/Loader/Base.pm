4538397bf4b14599e7b93d1355b31df532817658
Rafael Kitover <rkitover@gmail.com>
2011-02-01 02:42:53 +0000

    Import of RKITOVER/DBIx-Class-Schema-Loader-0.07006 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Schema-Loader
    gitpan-cpan-version:      0.07006
    gitpan-cpan-path:         RKITOVER/DBIx-Class-Schema-Loader-0.07006.tar.gz
    gitpan-cpan-author:       RKITOVER
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/Schema/Loader/Base.pm
+++ lib/DBIx/Class/Schema/Loader/Base.pm
@@ -25,7 +25,7 @@ use DBIx::Class ();
 use Class::Load 'load_class';
 use namespace::clean;
 
-our $VERSION = '0.07005';
+our $VERSION = '0.07006';
 
 __PACKAGE__->mk_group_ro_accessors('simple', qw/
                                 schema
@@ -1069,7 +1069,7 @@ sub _load_tables {
         local $self->{dump_directory} = $self->{temp_directory};
         $self->_reload_classes(\@tables);
         $self->_load_relationships($_) for @tables;
-#        $self->_relbuilder->cleanup; # this breaks perl 5.8.x
+        $self->_relbuilder->cleanup;
         $self->{quiet} = 0;
 
         # Remove that temp dir from INC so it doesn't get reloaded
