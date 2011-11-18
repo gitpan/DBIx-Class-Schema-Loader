a6f6255c207022190fffbdc5f4b4d9cc80d877f5
Rafael Kitover <rkitover@gmail.com>
2011-11-18 17:52:31 +0000

    Import of RKITOVER/DBIx-Class-Schema-Loader-0.07014 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Schema-Loader
    gitpan-cpan-version:      0.07014
    gitpan-cpan-path:         RKITOVER/DBIx-Class-Schema-Loader-0.07014.tar.gz
    gitpan-cpan-author:       RKITOVER
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/Schema/Loader/Base.pm
+++ lib/DBIx/Class/Schema/Loader/Base.pm
@@ -28,7 +28,7 @@ use List::MoreUtils qw/all any firstidx uniq/;
 use File::Temp 'tempfile';
 use namespace::clean;
 
-our $VERSION = '0.07013';
+our $VERSION = '0.07014';
 
 __PACKAGE__->mk_group_ro_accessors('simple', qw/
                                 schema
@@ -1515,7 +1515,7 @@ sub _load_tables {
 
                 delete $self->monikers->{$_->sql_name} for @$tables;
 
-                my $moniker_parts = $self->{moniker_parts};
+                my $moniker_parts = [ @{ $self->moniker_parts } ];
 
                 my $have_schema   = 1 if any { $_ eq 'schema'   } @{ $self->moniker_parts };
                 my $have_database = 1 if any { $_ eq 'database' } @{ $self->moniker_parts };
